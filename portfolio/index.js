d1393f39450da58ff47246a4430ff6c537cc9679
Dinis <d.yudenka@yandex.by>
2022-01-24 04:15:39 +0300

    feat: add a burger menu button

--- portfolio/index.js
+++ portfolio/index.js
@@ -3,3 +3,9 @@ console.log(`
 Отметка за portfolio#1 - 100 балла(ов).
 
 `)
+
+const hamburger = document.querySelector('.header__btn-burger');
+
+        hamburger.addEventListener('click', () => {
+            hamburger.classList.toggle('open');
+        });
