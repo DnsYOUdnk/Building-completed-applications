94598157f9fc981bd323100756cdba6c3e7807b4
Dinis <d.yudenka@yandex.by>
2022-01-24 12:11:58 +0300

    feat: implement the functions of the burger menu button

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,11 +1,18 @@
-console.log(`
-
-Отметка за portfolio#1 - 100 балла(ов).
-
-`)
+console.log(`Отметка за portfolio#1 - 100 балла(ов).`)
 
 const hamburger = document.querySelector('.header__btn-burger');
+const navMenu = document.querySelector('.nav__items');
 
         hamburger.addEventListener('click', () => {
             hamburger.classList.toggle('open');
+            navMenu.classList.toggle('openMenu');
         });
+
+const navLinks = document.querySelectorAll('.nav__link');
+        
+        navLinks.forEach((navLink) => {
+            navLink.addEventListener('click', () => {
+                hamburger.classList.toggle('open');
+                navMenu.classList.toggle('openMenu');
+            })
+        })
