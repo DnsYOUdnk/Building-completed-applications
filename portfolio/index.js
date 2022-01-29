af5e2b2bb8506b53a62ef05761704fe9e642081d
Dinis <d.yudenka@yandex.by>
2022-01-29 08:02:37 +0300

    feat: implement a new feature for the portfolio section

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,4 +1,6 @@
-console.log(`Отметка за portfolio#1 - 100 балла(ов).`)
+console.log(`Отметка за portfolio#1 - 100 баллов.`)
+
+// Реализация адаптивного меню
 
 const hamburger = document.querySelector('.header__btn-burger');
 const navMenu = document.querySelector('.nav__items');
@@ -17,8 +19,31 @@ const navLinks = document.querySelectorAll('.nav__link');
             })
         })
 
-console.log(`
-Моя самооценка по кроссчекформе для Portfolio#2 - 85 балла(ов)
-Отзыв по пунктам ТЗ:
-Все пункты выполнены полностью!
-`)
+console.log(`Отметка за portfolio#2 - 75 баллов.`)
+
+// Реализация функциональности сайта
+
+const portfolioParentBtns = document.querySelector('.portfolio__buttons'),
+      portfolioBtns = document.querySelectorAll('.portfolio__btn'),
+      portfolioImages = document.querySelectorAll('.portfolio__image');
+
+      portfolioParentBtns.addEventListener('click', (event) => {
+          console.log(event)
+        if(event.target.classList.contains('portfolio__btn')) {
+            portfolioBtns.forEach(item => item.classList.remove('active'))
+            event.target.classList.add('active')
+            portfolioImages.forEach((img, index) => img.src =`./assets/img/${event.target.dataset.period}/${index+1}.jpg`);
+        }
+      })
+
+const seasons = ['winter', 'spring', 'summer', 'autumn'];
+function preloadImages(seasons) {
+    seasons.forEach(item => {
+        for(let i = 1; i <= 6; i++) {
+            const img = new Image();
+            img.src = `./assets/img/${item}/${i}.jpg`;
+          }
+    })
+  }
+  preloadImages(seasons);
+
