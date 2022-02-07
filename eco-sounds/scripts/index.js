3519c01f184499684054cf1192f423a7f6f5f4ca
Dinis <d.yudenka@yandex.by>
2022-02-07 22:39:30 +0300

    feat: implement the background switching function

--- eco-sounds/scripts/index.js
+++ eco-sounds/scripts/index.js
@@ -1,10 +1,13 @@
-let headerElemnt = document.querySelector('.header__wrapper'),
-    headerBtns = document.querySelectorAll('.header__btn');
+let headerElemnts = document.querySelector('.header__wrapper'),
+    headerBtns = document.querySelectorAll('.header__btn'),
+    blockMain = document.querySelector('.main');
 
 
-    headerElemnt.addEventListener('click', event => {
+    headerElemnts.addEventListener('click', event => {
         if(event.target.classList.contains('header__btn')) {
             headerBtns.forEach(item => item.classList.remove('active'))
-            event.target.classList.add('active')
+            event.target.classList.add('active');
+            blockMain.style.backgroundImage = `url(./assets/img/${event.target.id}.jpg)`
+            console.log(event.target.id)
         }
     })
