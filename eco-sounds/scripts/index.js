41e65fd19847bbfef1ee990a073dd2c5587cc428
Dinis <d.yudenka@yandex.by>
2022-02-08 00:26:50 +0300

    fix: improved music playback when pressed

--- eco-sounds/scripts/index.js
+++ eco-sounds/scripts/index.js
@@ -1,27 +1,38 @@
 let headerElemnts = document.querySelector('.header__wrapper'),
     headerBtns = document.querySelectorAll('.header__btn'),
-    blockMain = document.querySelector('.main');
-
-let playAudio = false;
-
+    blockMain = document.querySelector('.main'),
+    playBtn = document.querySelector('.audio__btn');
+    
 const audio = new Audio();
-const audioPlayer = function (filePath) {
-    audio.src = `./assets/audio/${filePath}.mp3`;
-    audio.currentTime = 0;
-    if(playAudio) {
-        audio.play();
-    } else {
+
+let isPlayAudio = false,
+    filePath = 'forest';
+
+const playAudioBtn = function () {
+    if(isPlayAudio) {
+        isPlayAudio = false
         audio.pause()
+    } else {
+        isPlayAudio = true
+        audioPlayer();
     }
 }
 
+const audioPlayer = function () {
+    audio.src = `./assets/audio/${filePath}.mp3`;
+    audio.currentTime = 0;
+    audio.play();
+}
 
 headerElemnts.addEventListener('click', event => {
     if(event.target.classList.contains('header__btn')) {
         headerBtns.forEach(item => item.classList.remove('active'))
         event.target.classList.add('active');
+        isPlayAudio = true;
+        filePath = event.target.id;
+        audioPlayer();
         blockMain.style.backgroundImage = `url(./assets/img/${event.target.id}.jpg)`;
-        audioPlayer(event.target.id);
-        playAudio = true;
     }
 })
+
+playBtn.addEventListener('click', playAudioBtn)
