96ea7621bfaee300ad467715413e2646b1c7c71f
Dinis <d.yudenka@yandex.by>
2022-02-07 23:06:53 +0300

    feat: add music to navigation buttons

--- eco-sounds/scripts/index.js
+++ eco-sounds/scripts/index.js
@@ -2,12 +2,26 @@ let headerElemnts = document.querySelector('.header__wrapper'),
     headerBtns = document.querySelectorAll('.header__btn'),
     blockMain = document.querySelector('.main');
 
+let playAudio = false;
 
-    headerElemnts.addEventListener('click', event => {
-        if(event.target.classList.contains('header__btn')) {
-            headerBtns.forEach(item => item.classList.remove('active'))
-            event.target.classList.add('active');
-            blockMain.style.backgroundImage = `url(./assets/img/${event.target.id}.jpg)`
-            console.log(event.target.id)
-        }
-    })
+const audio = new Audio();
+const audioPlayer = function (filePath) {
+    audio.src = `./assets/audio/${filePath}.mp3`;
+    audio.currentTime = 0;
+    if(playAudio) {
+        audio.play();
+    } else {
+        audio.pause()
+    }
+}
+
+
+headerElemnts.addEventListener('click', event => {
+    if(event.target.classList.contains('header__btn')) {
+        headerBtns.forEach(item => item.classList.remove('active'))
+        event.target.classList.add('active');
+        blockMain.style.backgroundImage = `url(./assets/img/${event.target.id}.jpg)`;
+        audioPlayer(event.target.id);
+        playAudio = true;
+    }
+})
