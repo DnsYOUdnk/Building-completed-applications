111a97cdc23555c06487564ac9e2dcb5bfb56f3d
Dinis <d.yudenka@yandex.by>
2022-02-15 05:26:18 +0300

    feat: add API with random quotes

--- random-jokes/scripts/index.js
+++ random-jokes/scripts/index.js
@@ -1,5 +1,7 @@
 const mainBtn = document.querySelector('.random-jokes__btn');
-const mainImg = document.querySelector('.main__img')
+const mainImg = document.querySelector('.main__img');
+const randomText = document.querySelector('.random__text');
+let randText = '';
 
 let randomNum = function(min, max) {
     let resultNum = min + Math.random() * (max + 1 - min);
@@ -8,11 +10,33 @@ let randomNum = function(min, max) {
 
 mainBtn.addEventListener('click',(e) => {
     mainImg.classList.add('active');
+    getData()
+    randomText.innerText = randText;
     setTimeout(() => {
         mainImg.classList.remove('active');
         mainImg.src = `./assets/img/jokes-${randomNum(1,11)}.jpg`
     },900)
-    
-    
-
 })
+
+async function getData() {
+    const res = await fetch('https://type.fit/api/quotes');
+    const data = await res.json();
+    let newText = data[randomNum(0,data.length-1)].text;
+    randText = newText;
+}
+
+getData()
+
+function setLocalStorage() {
+    localStorage.setItem('newText', randText);
+  }
+window.addEventListener('beforeunload', setLocalStorage)
+
+
+function getLocalStorage() {
+    if(localStorage.getItem('newText')) {
+      const text = localStorage.getItem('newText');
+      randomText.innerText = text;
+    }
+}
+window.addEventListener('load', getLocalStorage)
