c5933fc31447e15d43f4f06b0de3dde9f969c517
Dinis <d.yudenka@yandex.by>
2022-02-16 03:36:10 +0300

    feat: add new json file and add language change

--- random-jokes/scripts/index.js
+++ random-jokes/scripts/index.js
@@ -1,6 +1,10 @@
 const mainBtn = document.querySelector('.random-jokes__btn');
 const mainImg = document.querySelector('.main__img');
 const randomText = document.querySelector('.random__text');
+const langBtns = document.querySelector('.header__lang');
+const langEnBtn = document.querySelector('.header__lang-en');
+const headTitle = document.querySelector('h1');
+
 
 let randomNum = function(min, max) {
     let resultNum = min + Math.random() * (max + 1 - min);
@@ -8,31 +12,60 @@ let randomNum = function(min, max) {
 }
 
 const audio = new Audio();
-
 const audioPlayer = function () {
     audio.src = `./assets/audio/track-${randomNum(1,5)}.mp3`;
     audio.currentTime = 0;
     audio.play();
 }
 
+const changeLang = function (id) {
+    if (id == 'langRu') {
+        headTitle.innerText = 'Случайные цитаты';
+        mainBtn.innerText = 'Нажми и вращай'
+    } else if (id == 'langEn') {
+        headTitle.innerText = 'Random quotes';
+        mainBtn.innerText = 'Tap and spin';
+    }
+}
+
+const dataEng = 'https://type.fit/api/quotes';
+const dataRu = './assets/json/quotes.json';
+
+async function getData(id) {
+    let url = id == 'langRu' ? dataRu : dataEng;
+    const res = await fetch(url);
+    const data = await res.json();
+    let newText = data[randomNum(0,data.length-1)].text;
+    randomText.innerText = newText;
+}
+
+langBtns.addEventListener('click', (event) => {
+    let arr = event.path[1].children;
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i].classList.contains('active')){
+            arr[i].classList.remove('active')
+        }
+    }
+    localStorage.setItem('lang',event.target.id);
+    event.target.classList.add('active');
+    changeLang(event.target.id);
+    getData(event.target.id);
+})
+
 mainBtn.addEventListener('click',(e) => {
     mainImg.classList.add('active');
     audioPlayer();
-    getData();
+    getData(localStorage.getItem('lang'));
     setTimeout(() => {
         mainImg.classList.remove('active');
         mainImg.src = `./assets/img/jokes-${randomNum(1,11)}.jpg`;
     },900)
 })
 
-async function getData() {
-    const res = await fetch('https://type.fit/api/quotes');
-    const data = await res.json();
-    let newText = data[randomNum(0,data.length-1)].text;
-    randomText.innerText = newText;
-}
-
-window.addEventListener('load', getData())
+window.addEventListener('load', () => {
+    let langOpt = localStorage.getItem('lang');
+    getData(langOpt);
+    changeLang(langOpt)
+})
 
 
-console.log('Прошу прощения за грязную работу, накидал все это дело в дикой в спешке...Не судите строго или судите дело ваше, в общем надеюсь что кто-то угорнет. А вообще ребята если вы разобрались в теме с API напишите подробнее все норм я сделал или это какая-то дичь. Спасибо большое')
