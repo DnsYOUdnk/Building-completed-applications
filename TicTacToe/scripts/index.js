06272d4d1c6a44b7224f5c0220fb913365712cf9
Dinis <d.yudenka@yandex.by>
2022-02-22 14:59:19 +0300

    feat: implemented the function of starting a new game

--- TicTacToe/scripts/index.js
+++ TicTacToe/scripts/index.js
@@ -3,16 +3,31 @@ const areaCells = document.querySelectorAll('.area__cell');
 const restartBtn = document.querySelector('.pop_up__close__btn');
 const popUpContent = document.querySelector('.pop_up__content');
 const popUpwrapper = document.querySelector('#pop__up');
+const winConfetti = document.querySelector('.confetti');
 let editMove = 0;
 let winner = '';
 
 
-areaGame.addEventListener('click', (event) => {
-    if(event.target.className == 'area__cell' && !event.target.innerText) {
-        editMove%2 == 0 ? event.target.innerText = 'X' : event.target.innerText = '0'; 
+areaGame.addEventListener('mousedown', (event) => {
+    if(event.target.className == 'area__cell') {
+        editMove%2 == 0 ? event.target.innerText = 'X' : event.target.innerText = '0';
+        event.target.classList.add('lock');
+        event.target.removeAttribute("style"); 
         editMove++;
     }
     victoryCheck();
+})
+areaGame.addEventListener('mouseover', (event) => {
+    if(event.target.className != 'area__cell lock' && !event.target.innerText) {
+        editMove%2 == 0 ? event.target.innerText = 'X' : event.target.innerText = '0';
+        event.target.style.color = 'rgba(0, 0, 0, 0.37)'; 
+    }
+})
+
+areaGame.addEventListener('mouseout', (event) => {
+    if(event.target.className != 'area__cell lock' && event.target.innerText) {
+        editMove%2 == 0 ? event.target.innerText = '' : event.target.innerText = ''; 
+    }
 })
 
 const victoryCheck = function() {
@@ -31,26 +46,39 @@ const victoryCheck = function() {
     arrResult.forEach(item => {
         if(areaCells[item[0]].innerText == 'X' && areaCells[item[1]].innerText == 'X' && areaCells[item[2]].innerText == 'X') {
             winner = 'X'
-            getResult('Победил: ' + winner)
+            getResult(winner, item)
         } else if(areaCells[item[0]].innerText == '0' && areaCells[item[1]].innerText == '0' && areaCells[item[2]].innerText == '0') {
             winner = '0'
-            getResult('Победил: ' + winner)
+            getResult(winner, item)
         }
     })
 
+    if(editMove == 9) {
+        winner = !winner ? 'ничья' : winner;
+        getResult(winner, item)
+    }
+
     if(!winner && editMove == 9) {
         winner = 'ничья'
-        getResult('Вышла: ' + winner)
     }
 }
 
-const getResult = function(winner) {
-    popUpwrapper.classList.add('active')
-    popUpContent.innerText = winner
-    areaCells.forEach(item => {
-        console.log(item.innerText = '')
-    })
-    console.log(winner)
+const getResult = function(winner, arr) {
+    popUpwrapper.classList.add('active');
+    winConfetti.classList.add('active');
+    
+    popUpContent.innerHTML = `<h3>В этой игре победил:</h3>
+                                    <p>${winner}</p>
+                            <p>Количество ходов в игре: ${editMove} </p>`;
 }
 
-window.addEventListener('load', alert('Ребята работа не сделана до конца, если не сложно проверьте завтра'))
+restartBtn.addEventListener('click', () => {
+    popUpwrapper.classList.remove('active');
+    winConfetti.classList.remove('active');
+    editMove = 0;
+    winner = '';
+    areaCells.forEach(item => {
+        item.innerText = '';
+        item.classList.remove('lock') 
+    })
+})
