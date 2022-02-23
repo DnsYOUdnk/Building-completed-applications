43c8ae4fe8dc2bcd10235e89e8b3b96c77ff027e
Dinis <d.yudenka@yandex.by>
2022-02-23 07:35:22 +0300

    feat: add new audio file and add sound to the interface

--- TicTacToe/scripts/index.js
+++ TicTacToe/scripts/index.js
@@ -5,10 +5,26 @@ const popUpContent = document.querySelector('.pop_up__content');
 const windowPopUp = document.querySelector('#pop__up');
 const winConfetti = document.querySelector('.confetti');
 const getStaticBtn = document.querySelector('.header__table-btn');
+const getSettingBtn = document.querySelector('.header__setting-btn');
 
 let editMove = 0;
 let winner = '';
 let staticArr = [];
+
+let audio = new Audio();
+let audioMusic = new Audio();
+
+let playNowAudio = function(trackAudio) {
+    audio.src = `./assets/audio/${trackAudio}.mp3`;
+    audio.currentTime = 0;
+    audio.play();
+}
+
+let playAudioMusic = function(trackAudio) {
+    audioMusic.src = `./assets/audio/${trackAudio}.mp3`;
+    audioMusic.currentTime = 0;
+    audioMusic.play();
+}
 
 areaGame.addEventListener('mousedown', (event) => {
     if(event.target.className == 'area__cell' && event.target.innerText) {
@@ -17,6 +33,7 @@ areaGame.addEventListener('mousedown', (event) => {
         event.target.removeAttribute("style"); 
         editMove++;
     }
+    playNowAudio('addX0');
     victoryCheck();
 })
 
@@ -63,16 +80,17 @@ const victoryCheck = function() {
         staticArr.push(0)
         getResult(winner,[0,1,2,3,4,5,6,7,8])
     }
-    if(staticArr.length > 10) staticArr.shift();
 }
 
 const getResult = function(winner, arr) {
     arr.forEach((item) => {
         if(arr.length > 3) {
             areaCells[item].style.background = '#0e56a8c7';
-            areaCells[item].classList.add('active')
+            areaCells[item].classList.add('active');
+            playNowAudio('gameover');
         } else {
-            areaCells[item].classList.add('active')
+            areaCells[item].classList.add('active');
+            playNowAudio('congratulation');
         }
     })
     windowPopUp.classList.add('active__on__pop');
@@ -83,21 +101,23 @@ const getResult = function(winner, arr) {
                             <p>Количество ходов в игре: ${editMove} </p>`;
     localStorage.setItem('resultStatic', JSON.stringify(staticArr));
     recNewData(staticArr);
+    audioMusic.pause();
 }
 
 const recNewData = function(dataArr) {
+    if(staticArr.length > 10) staticArr.shift();
     const staticList = document.querySelector('.menu__table__content');
     let list = '';
-    dataArr.forEach((item, index) => {
+    
+    for(let i=dataArr.length-1; i >= 0; i--) {
         list += `
                 <div class="table__content__list">
-                    <div class="list__item">${index+1}</div>
-                    <div class="list__item">${item ? item : 'Результат игры'}</div>
-                    <div class="list__item">${item ? 'Победитель' : 'Ничья'}</div>
+                    <div class="list__item">${i+1}</div>
+                    <div class="list__item">${dataArr[i] ? dataArr[i] : 'Результат игры'}</div>
+                    <div class="list__item">${dataArr[i] ? 'Победитель' : 'Ничья'}</div>
                 </div>
             `
-    })
-
+    }
     staticList.innerHTML = list;
 }
 
@@ -113,19 +133,29 @@ restartBtn.addEventListener('click', () => {
         item.classList.remove('active'); 
         item.removeAttribute("style"); 
     })
+    playNowAudio('addButton');
+    playAudioMusic('audioTrack');
 })
 
 getStaticBtn.addEventListener('click', () => {
     windowPopUp.classList.add('active__on__table');
+    playNowAudio('addButton');
+})
+
+getSettingBtn.addEventListener('click', () => {
+    windowPopUp.classList.add('active__on__setting');
+    playNowAudio('addButton');
 })
 
 windowPopUp.addEventListener('click', (event) => {
-    if(event.target.id == 'menu__table__close' || event.target.id == 'popUp_wrap') {
-        windowPopUp.classList.remove('active__on__table');
+    if(event.target.id == 'menu__table__close' || event.target.id == 'popUp_wrap' || event.target.id == 'menu__setting__close') {
+        windowPopUp.classList.remove('active__on__table','active__on__setting');
+        playNowAudio('addButton');
     }
 })
 
 window.addEventListener('load', () => {
+    if(!localStorage.getItem('resultStatic')) return
     staticArr = JSON.parse(localStorage.getItem('resultStatic'));
-    recNewData(staticArr)
+    recNewData(staticArr);
 })
